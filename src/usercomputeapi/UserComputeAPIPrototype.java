e9af162a75e1b92c1d14d95162b195fe278c5a3d
Andradem11 <marcoandra11@outlook.com>
2025-09-15 13:15:34 -0400

    Update UserComputeAPIPrototype.java

--- src/usercomputeapi/UserComputeAPIPrototype.java
+++ src/usercomputeapi/UserComputeAPIPrototype.java
@@ -6,9 +6,9 @@ public class UserComputeAPIPrototype {
 	@NetworkAPIPrototype
 	public void prototype(UserComputeAPI user, DataSource source, String delimiter) {
 		
-		if(delimiter==null || delimiter.isBlank()) delimiter=";";
-		ComputeRequest request=new ComputeRequest(source, delimiter);
-		ComputeResponse response= user.computeSumOfPrimes(request);
+		if(delimiter == null || delimiter.isBlank()) delimiter = ";";
+		ComputeRequest request = new ComputeRequest(source, delimiter);
+		ComputeResponse response = user.computeSumOfPrimes(request);
 		if(response.isSuccess()) {
 			System.out.println("Sum of primes: "+ response.getSum());
 		}else {

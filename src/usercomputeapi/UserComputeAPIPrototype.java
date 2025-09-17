6f3adb80b62bd44c7be2149a9203c6eb3caf088f
Andradem11 <marcoandra11@outlook.com>
2025-09-16 20:34:10 -0400

    Check2 api/prototype
    * userComputeapi interface and prototype.
    
    created userCompute interface wit methods to set input, output and delimiters, also added the prototype to show an example of usage.
    
    * storageCompute interface and prototype
    
    Created storageComputeAPI interface with methods that read a list of inputs and writes formatted output, and the prototype demostrating how to read input and write the output
    
    * ComputeEngineAPI interface and prototype
    
    Created an interface with methods for single and multiple inputs, added a prototype showing an example for each.
    
    * update code
    
    contains request and response files
    
    * updated code
    
    * pass checkstyle
    
    * Update StorageComputeAPIPrototype.java
    
    * Update UserComputeAPIPrototype.java
    
    * checkstyle
    
    * updated code
    
    the sum gets stored in a list and written to the output the user gives instead of just printing it. I also switched from using booleans to enums for status.
    
    * fixed code so prototypes only contain 1 parameter
    
    * reference prototype to its api

--- src/usercomputeapi/UserComputeAPIPrototype.java
+++ src/usercomputeapi/UserComputeAPIPrototype.java
@@ -8,20 +8,19 @@ import project.annotations.NetworkAPIPrototype;
 public class UserComputeAPIPrototype {
 	@NetworkAPIPrototype
 	 public List<Integer> prototype(UserComputeAPI user) {
+//		reference to API
+		UserComputeAPI api = user;
+		
 //		variables (input and output)
-		DataSource source= null;
+		DataSource source = null;
 		String delimiter = ";";
-		OutputSource output= null;
 
 //	     request form compute engine
 	     ComputeRequest request = new ComputeRequest(source, delimiter);
-	     ComputeResponse response = null;
-	     if (user != null ) {
-	    	 response=user.computeSumOfPrimes(request);
-	    	 
-	     }
+	     ComputeResponse response = api.computeSumOfPrimes(request);
+	     
 //	     store sum in a list
-	     List<Integer> results=new ArrayList<>();
+	     List<Integer> results = new ArrayList<>();
 	     if(response != null && response.isSuccess()) {
 	    	 results.add(response.getSum());
 	     } else {

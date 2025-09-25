489d99f3f10fddec1586e3c717b33180dd1f3e3f
Andradem11 <marcoandra11@outlook.com>
2025-09-25 10:55:57 -0400

    Updated test Folder (#34)

--- test/integration/TestComputeEngineIntegration.java
+++ test/integration/TestComputeEngineIntegration.java
@@ -2,40 +2,26 @@ package integration;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
 import java.util.List;
 
 import computeengineapi.ComputeEngineAPI;
 import computeengineapi.ComputeEngineImpl;
+import storagecomputeapi.TestStorageComputeAPIImpl;
 
 
 public class TestComputeEngineIntegration {
 	
 	@Test
 	public void testComputeEngine() {
-//		create input and output
-		TestInput input = new TestInput(Arrays.asList(1, 10, 25));
-		TestOutput output = new TestOutput();
-		
-//		create a data store
-		TestDataStore data = new TestDataStore(input, output);
-		
-//		read input from data store
-		List<Integer> inputNum = data.readInput();
-		
-//		compute engine implementation
-		ComputeEngineAPI engine = new ComputeEngineImpl();
-		
-//		call input and store it to string
-		for(Integer num : inputNum) {
-			int result = engine.computeSum(Arrays.asList(num));
-			data.writeOut(Arrays.asList(String.valueOf(result)));
-		}
-		
-//		output
-		List<String> out = output.getOutput();
-		assertEquals(Arrays.asList("0","0","0"), out);
-	}
+		TestStorageComputeAPIImpl dataStore = new TestStorageComputeAPIImpl();
+	    ComputeEngineAPI engine = new ComputeEngineImpl();
 
+	    for (Integer num : dataStore.readInput()) {
+	        int result = engine.computeSum(List.of(num));
+	        dataStore.writeOutput(List.of(result));
+	    }
+
+	    assertEquals(List.of("0", "0", "0"), dataStore.getTestOutput());
+	}
 }
+

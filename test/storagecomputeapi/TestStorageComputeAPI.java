c363a51016e17c95d7addc275aaa4a9ca9b3ef99
Andradem11 <marcoandra11@outlook.com>
2025-09-25 13:50:36 -0400

    Update TestStorageComputeAPI.java

---
+++ test/storagecomputeapi/TestStorageComputeAPI.java
@@ -0,0 +1,33 @@
+package storagecomputeapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TestStorageComputeAPI {
+	@Test
+	public void smokeTestStorageCompute() {	
+//		create mock of API
+		StorageComputeAPI mockStorage = Mockito.mock(StorageComputeAPI.class);
+		
+//		when readInput call, print 1, 2
+		when(mockStorage.readInput()).thenReturn(Arrays.asList(1, 2));
+		
+//		when writeOutput call, print success
+		when(mockStorage.writeOutput(any()))
+		.thenReturn(new StorageResponse(StorageResponse.Status.SUCCESS, "MOCKED WRITE"));
+		
+//		run the Prototype
+		StorageComputeAPIPrototype prototype = new StorageComputeAPIPrototype();
+		prototype.prototype(mockStorage);
+		
+		assertEquals(1,1);	
+	}
+
+
+}

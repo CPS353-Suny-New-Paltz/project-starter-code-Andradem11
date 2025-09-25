6dec6ef1b6d2f158d8c94d83ef860d1849cee78a
Andradem11 <marcoandra11@outlook.com>
2025-09-25 17:32:17 -0400

    Create SmokeTestStorage.java

---
+++ test/storagecomputeapi/SmokeTestStorage.java
@@ -0,0 +1,11 @@
+package storagecomputeapi;
+import org.junit.jupiter.api.Test;
+
+public class SmokeTestStorage {
+	
+	@Test
+	public void StorageSmokeTestImpl() {
+		TestStorageComputeAPIImpl api = new TestStorageComputeAPIImpl();
+	}
+
+}

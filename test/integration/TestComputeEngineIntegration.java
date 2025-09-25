878ced23216fe4bd9f048d27c0cfcb9429ca5b73
Andradem11 <marcoandra11@outlook.com>
2025-09-25 15:04:32 -0400

    Check3re2 (#45)
    * passing test
    
    * passing test

--- test/integration/TestComputeEngineIntegration.java
+++ test/integration/TestComputeEngineIntegration.java
@@ -7,6 +7,8 @@ import java.util.List;
 import computeengineapi.ComputeEngineAPI;
 import computeengineapi.ComputeEngineImpl;
 import storagecomputeapi.TestStorageComputeAPIImpl;
+import usercomputeapi.ComputeRequest;
+import usercomputeapi.ComputeResponse;
 import usercomputeapi.UserComputeAPI;
 import usercomputeapi.UserComputeImpl;
 
@@ -15,14 +17,17 @@ public class TestComputeEngineIntegration {
 	
 	@Test
 	public void testComputeEngine() {
-		TestStorageComputeAPIImpl dataStore = new TestStorageComputeAPIImpl();
-        ComputeEngineAPI engine = new ComputeEngineImpl();
-        UserComputeAPI user = new UserComputeImpl(); 
+		 TestStorageComputeAPIImpl dataStore = new TestStorageComputeAPIImpl();
+	     ComputeEngineAPI engine = new ComputeEngineImpl();
+	     UserComputeAPI user = new UserComputeImpl();
 
-	    for (Integer num : dataStore.readInput()) {
-	        int result = engine.computeSum(List.of(num));
-	        dataStore.writeOutput(List.of(result));
-	    }
+	     ComputeRequest request = new ComputeRequest(null, ";");
+	     ComputeResponse response = user.computeSumOfPrimes(request);
+
+	        for (Integer num : dataStore.readInput()) {
+	            int result = engine.computeSum(List.of(num));
+	            dataStore.writeOutput(List.of(result));
+	        }
 
 	    assertEquals(List.of("0", "0", "0"), dataStore.getTestOutput());
 	}

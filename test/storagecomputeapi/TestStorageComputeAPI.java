2c3fa59a3f6d5c6750a08e3a226d4395c6f7d17e
Andradem11 <marcoandra11@outlook.com>
2025-09-25 21:52:20 -0400

    Update TestStorageComputeAPI.java

--- test/storagecomputeapi/TestStorageComputeAPI.java
+++ test/storagecomputeapi/TestStorageComputeAPI.java
@@ -2,20 +2,24 @@ package storagecomputeapi;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class TestStorageComputeAPI {
-	@Test
-	public void smokeTestStorageCompute() {	
+    @Test
+    public void smokeTestStorageCompute() {
         StorageComputeAPI storage = new StorageComputeImpl();
-        
-        List<Integer> input = storage.readInput(); 
+        List<Integer> input = storage.readInput();
         StorageResponse response = storage.writeOutput(List.of(1, 2, 3));
-
         assertTrue(response.getStatus() == StorageResponse.Status.FAIL);
     }
 
-}
+    @Test
+    public void testStorageComputeImplInstantiation() {
+        StorageComputeImpl impl = new StorageComputeImpl();
+        assertTrue(impl.readInput().isEmpty());
+    }
+} 
+
+
 
 

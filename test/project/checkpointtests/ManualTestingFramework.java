4f527e5bbd813cff1cffdf8c62453d211ad6244d
Andradem11 <marcoandra11@outlook.com>
2025-10-02 14:24:41 -0400

    Compute Engine, User and  Storage Implementation
    implemented ComputeEngine, StorageCompute and UserCompute classes. added a manualTestingFramework to test manually

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -1,14 +1,11 @@
 package project.checkpointtests;
 
-import edu.softwareeng.sample.ComputationCoordinator;
-import edu.softwareeng.sample.ComputeEngine;
-import edu.softwareeng.sample.ComputeEngineImpl;
-import edu.softwareeng.sample.ComputeRequest;
-import edu.softwareeng.sample.CoordinatorImpl;
-import edu.softwareeng.sample.DataStore;
-import edu.softwareeng.sample.DataStoreImpl;
-import edu.softwareeng.sample.FileInputConfig;
-import edu.softwareeng.sample.FileOutputConfig;
+import computeengineapi.ComputeEngineImpl;
+import storagecomputeapi.StorageComputeImpl;
+import usercomputeapi.UserComputeImpl;
+import usercomputeapi.ComputeRequest;
+import usercomputeapi.ComputeResponse;
+import usercomputeapi.DataSource;
 
 public class ManualTestingFramework {
     
@@ -16,15 +13,31 @@ public class ManualTestingFramework {
     public static final String OUTPUT = "manualTestOutput.txt";
 
     public static void main(String[] args) {
-        // TODO 1:
-        // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
-        // of all 3 APIs
-        //
-        // TODO 2:
-        // Run a computation with an input file of <root project dir>/manualTestInput.txt
-        // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
-        //
-        //
+    	
+//    	TODO 1:
+//    	Instantiate a real (ie, class definition lives in the src/ folder) implementation 
+//      of all 3 APIs
+    	ComputeEngineImpl engine = new ComputeEngineImpl();
+    	StorageComputeImpl storage = new StorageComputeImpl();
+    	UserComputeImpl coordinator = new UserComputeImpl(storage, engine);
+    	
+//      TODO 2:
+//      Run a computation with an input file of <root project dir>/manualTestInput.txt
+//      and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
+    	DataSource source = new DataSource() {
+    		@Override
+    		public int getLimit() {
+    			return 50;
+    		}
+    	};
+    	ComputeRequest request = new ComputeRequest(source, ",");
+    	ComputeResponse response = coordinator.computeSumOfPrimes(request);
+    	
+    	if(response.isSuccess()) {
+    		System.out.println("Computation succeded. Sum; "+ response.getSum());
+    	}else {
+    		System.out.println("Computation failed.");
+    	}
         // Helpful hint: the working directory of this program is <root project dir>,
         // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
         // need to (and should not) actually create those files in your repo

33e57a28ce0591491c007ce6aeed9b8adfc5907d
Andradem11 <marcoandra11@outlook.com>
2025-10-02 15:36:53 -0400

    Update ManualTestingFramework.java

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -1,5 +1,12 @@
 package project.checkpointtests;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import computeengineapi.ComputeEngineImpl;
 import storagecomputeapi.StorageComputeImpl;
 import usercomputeapi.UserComputeImpl;
@@ -14,30 +21,53 @@ public class ManualTestingFramework {
 
     public static void main(String[] args) {
     	
-//    	TODO 1:
-//    	Instantiate a real (ie, class definition lives in the src/ folder) implementation 
-//      of all 3 APIs
+//    	TODO 1: Instantiate  all 3 APIs
     	ComputeEngineImpl engine = new ComputeEngineImpl();
     	StorageComputeImpl storage = new StorageComputeImpl();
     	UserComputeImpl coordinator = new UserComputeImpl(storage, engine);
     	
-//      TODO 2:
-//      Run a computation with an input file of <root project dir>/manualTestInput.txt
-//      and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
-    	DataSource source = new DataSource() {
-    		@Override
-    		public int getLimit() {
-    			return 50;
+//      TODO 2: Run a computation with an input 
+    	List<Integer> input = new ArrayList<>();
+    	try(BufferedReader reader = new BufferedReader(new FileReader(INPUT))) {
+    		String line = reader.readLine();
+    		while ((line != null)) {
+    			 String data = line.trim(); 
+    			 if(!data.isEmpty()) {
+    				 input.add(Integer.parseInt(data));
+    			 }
     		}
-    	};
-    	ComputeRequest request = new ComputeRequest(source, ",");
-    	ComputeResponse response = coordinator.computeSumOfPrimes(request);
-    	
-    	if(response.isSuccess()) {
-    		System.out.println("Computation succeded. Sum; "+ response.getSum());
-    	}else {
-    		System.out.println("Computation failed.");
+    		
+    	}catch (IOException e) {
+    		e.printStackTrace();
+    		
     	}
+    	
+    	List<Integer> result = new ArrayList<>();
+    	for( Integer n : input) {
+    		DataSource source = new DataSource() {
+        		@Override
+        		public int getLimit() {
+        			return n;
+        		}
+    		};
+    		ComputeRequest request = new ComputeRequest(source, ",");
+        	ComputeResponse response = coordinator.computeSumOfPrimes(request);
+        	if(response.isSuccess()) {
+        		result.add(response.getSum());
+        	}else {
+        		result.add(0);
+        	}
+    	}
+    	try (FileWriter writer = new FileWriter(OUTPUT)) {
+            for (int j = 0; j < result.size(); j++) {
+                writer.write(String.valueOf(result.get(j)));
+                if (j < result.size() - 1) {
+                    writer.write(",");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         // Helpful hint: the working directory of this program is <root project dir>,
         // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
         // need to (and should not) actually create those files in your repo

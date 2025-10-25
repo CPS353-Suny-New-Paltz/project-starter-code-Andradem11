f67bd5beb56c3af5e510af9a5881d774fa19c1a5
Andradem11 <marcoandra11@outlook.com>
2025-10-25 12:31:33 -0400

    Checkpoint5 test valid (#105)
    * testing validation for coordinator and storage
    
    -The test coordinator validation covers null request, null Datasource, and negative numbers inputs.
    -The test storage validation cover readInput with null, blank or non-existing files, and writeOutput covers null and empty for data and output path.
    
    * Update TestUserComputeAPI.java
    
    Improve failure assertion

--- test/storagecomputeapi/TestStorageComputeAPI.java
+++ test/storagecomputeapi/TestStorageComputeAPI.java
@@ -1,5 +1,6 @@
 package storagecomputeapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -31,5 +32,52 @@ public class TestStorageComputeAPI {
         tempFile.toFile().deleteOnExit();
         
         assertTrue(impl.readInput(tempFile.toString()).isEmpty());
+    }
+    
+    
+//    validation test
+    @Test
+    public void testReadInputValidation() {
+    	StorageComputeAPI storage = new StorageComputeImpl();
+    	
+//    	null should return empty list
+    	List<Integer> r1 = storage.readInput(null);
+    	assertTrue(r1.isEmpty(), "readInput(null) returns empty list");
+    	
+//    	empty should return empty list
+    	List<Integer> r2 = storage.readInput("  ");
+    	assertTrue(r2.isEmpty(), "readInput(blank) returns empty list");
+    	
+//    	non-existing file should return empty list
+    	List<Integer> r3 = storage.readInput("noFile.txt");
+    	assertTrue(r3.isEmpty(), "readInput(no File) returns empty list");
+    }
+    @Test
+    public void testWriteOutputValidation() throws IOException {
+    	StorageComputeAPI storage = new StorageComputeImpl();
+    	
+//    	temporary file
+    	Path tempFile = Files.createTempFile("OutputTest","");
+    	tempFile.toFile().deleteOnExit();
+    	
+//    	null data returns FAIL
+    	StorageResponse r1 = storage.writeOutput(null, tempFile.toString());
+    	assertEquals(StorageResponse.Status.FAIL, r1.getStatus(),
+    			"writeOutput(null data) should return FAIL");
+    	
+//    	empty list returns FAIL
+    	StorageResponse r2 = storage.writeOutput(List.of(), tempFile.toString());
+    	assertEquals(StorageResponse.Status.FAIL, r2.getStatus(),
+    			"writeOutput(empty list) should return FAIL");
+    	
+//    	null path returns FAIL
+    	StorageResponse r3 = storage.writeOutput(List.of(2,5,10), null);
+    	assertEquals(StorageResponse.Status.FAIL, r3.getStatus(),
+    			"writeOutput(null path) should return FAIL");
+    	
+//    	empty path returns FAIL
+    	StorageResponse r4 = storage.writeOutput(List.of(2,5,10), "  ");
+    	assertEquals(StorageResponse.Status.FAIL, r1.getStatus(),
+    			"writeOutput(empty path) should return FAIL");
     }
 } 
--- test/usercomputeapi/TestUserComputeAPI.java
+++ test/usercomputeapi/TestUserComputeAPI.java
@@ -46,7 +46,40 @@ public class TestUserComputeAPI {
         ComputeRequest request = new ComputeRequest(null, ";");
         ComputeResponse response = user.computeSumOfPrimes(request);
 
-        assertTrue(response.getStatus() == ComputeResponse.Status.FAIL);
+        assertEquals(ComputeResponse.Status.FAIL, response.getStatus(),
+                "Expected FAIL when DataSource is null");
+	}
+	
+//	validation test
+	@Test
+	public void testComputeSumOfPrimesValidation() {
+		StorageComputeAPI storage = new StorageComputeImpl();
+		ComputeEngineAPI engine = new ComputeEngineImpl();
+		UserComputeAPI user = new UserComputeImpl(storage, engine);
+		
+//		null request
+		ComputeResponse r1 = user.computeSumOfPrimes(null);
+		assertEquals(ComputeResponse.Status.FAIL, r1.getStatus(), 
+				"null request should return FAIL.");
+		
+//		null DataSource 
+		ComputeRequest requestNull = new ComputeRequest(null,";");
+		ComputeResponse r2 = user.computeSumOfPrimes(requestNull);
+		assertEquals(ComputeResponse.Status.FAIL, r2.getStatus(),
+				"null DataSource should return FAIL");
+		
+//		negative number
+		ComputeRequest requestNegative = new ComputeRequest(new DataSource() {
+	        @Override
+	        public int getLimit() {
+	            return -10;
+	        }
+	    }, ";");
+		ComputeResponse r3 = user.computeSumOfPrimes(requestNegative);
+		assertEquals(ComputeResponse.Status.FAIL, r3.getStatus(), 
+				"negative number should return FAIL");
+		
 	}
 
 }
+

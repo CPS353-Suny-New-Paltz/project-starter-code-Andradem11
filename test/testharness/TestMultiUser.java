4f6793ab9140329e70e2fc4c91dbd5a1192ae782
Andradem11 <marcoandra11@outlook.com>
2025-11-11 22:06:24 -0500

    Testing Harness (#109)
    * Testing Harness
    
    -Completed TODOs in TestMultiUser and TestUser.
    -Created testInputFile.test with numbers 1,15,10,5,2,3,8.
    -Verified that TestMultiUser runs successfully.
    
    * Update TestMultiUser.java
    
    * fixing to pass checkstyle
    
    * checkstyle
    
    * Update TestMultiUser.java
    
    Changed the test to use Files.createTempFile() instead of fixed file names to avoid conflicts
    Added code to delete temp files after the test
    Updated multi-threaded part to use these temp files safely

---
+++ test/testharness/TestMultiUser.java
@@ -0,0 +1,108 @@
+package testharness;
+
+import computeengineapi.ComputeEngineImpl;
+import storagecomputeapi.StorageComputeImpl;
+import usercomputeapi.UserComputeAPI;
+import usercomputeapi.UserComputeImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMultiUser {
+	
+	// TODO 1: change the type of this variable to the name you're using for your @NetworkAPI
+	// interface
+	private UserComputeAPI coordinator;
+	
+	@BeforeEach
+	public void initializeComputeEngine() {
+		coordinator = new UserComputeImpl(new StorageComputeImpl(), new ComputeEngineImpl());
+		
+		//TODO 2: create an instance of the implementation of your @NetworkAPI; this is the component
+		// that the user will make requests to
+		// Store it in the 'coordinator' instance variable
+	}
+	public void cleanup() {
+    }
+	@Test
+	public void compareMultiAndSingleThreaded() throws Exception {
+		int nthreads = 4;
+		List<TestUser> testUsers = new ArrayList<>();
+		for (int i = 0; i < nthreads; i++) {
+			testUsers.add(new TestUser(coordinator));
+		}
+		
+		// Run single threaded
+		List<Path> singleThreadFilePrefix = new ArrayList<>();
+        for (int i = 0; i < nthreads; i++) {
+            Path temp = Files.createTempFile("singleThreadOut", ".tmp");
+            singleThreadFilePrefix.add(temp);
+            testUsers.get(i).run(temp.toString());
+        }
+		
+		// Run multi threaded
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        List<Future<?>> results = new ArrayList<>();
+        List<Path> multiThreadFilePrefix = new ArrayList<>();
+        for (int i = 0; i < nthreads; i++) {
+            final Path temp = Files.createTempFile("multiThreadOut", ".tmp");
+            multiThreadFilePrefix.add(temp);
+            final TestUser testUser = testUsers.get(i);
+            results.add(threadPool.submit(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    testUser.run(temp.toString());
+                    return null;
+                }
+            }));
+        }
+		
+		for (Future<?> future : results) {
+            try {
+                future.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+		
+		
+		// Check that the output is the same for multi-threaded and single-threaded
+		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix);
+		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix);
+		Assertions.assertEquals(singleThreaded, multiThreaded);
+		
+//		Clean up temp files
+        for (Path p : singleThreadFilePrefix) {
+        	Files.deleteIfExists(p);
+        }
+        for (Path p : multiThreadFilePrefix) {
+        	Files.deleteIfExists(p);
+        }
+	}
+
+	private List<String> loadAllOutput(List<Path> paths) throws IOException {
+        List<String> result = new ArrayList<>();
+        for (Path p : paths) {
+            result.addAll(Files.readAllLines(p));
+        }
+        return result;
+    }
+	@Test
+    public void smokeTest() {
+//        List<String> requests = List.of("test1", "test2", "test3");
+//        List<String> results = UserComputeImpl.processFile(requests);
+//        Assertions.assertEquals(requests.size(), results.size());
+    }
+}
